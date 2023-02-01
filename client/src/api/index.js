fe4c1408d106ed6c701d5cd31cab3b334ae93f13
LucyLu23 <114943897+LucyLu23@users.noreply.github.com>
2023-02-01 16:20:46 -0700

    deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000/posts/'; //go to backend routes //5000
+const url = 'https://finalmernapplucy.onrender.com/posts/'; //go to backend routes //5000
 
 export const fetchPosts =() => axios.get(url);
 //export const fetchPost = (id) => axios.patch(`${url}/${id}`); 
@@ -10,3 +10,5 @@ export const updatePost =(id, updatedPost) =>axios.patch(`${url}/${id}`, updated
 export const deletePost = (id)=>axios.delete(`${url}/${id}`);
 export const likePost = (id) => axios.patch (`${url}/${id}/likePost`);
 export const dislikePost = (id) => axios.patch (`${url}/${id}/dislikePost`);
+
+//https://finalmernapplucy.onrender.com
